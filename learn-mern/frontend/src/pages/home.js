a734b40408e42bf76f1c7e9e6c731b07dff0c20a
Logan Bramwell <loganbramwellqu@gmail.com>
2023-06-27 17:46:25 -0700

    using workoutdetails component

--- learn-mern/frontend/src/pages/home.js
+++ learn-mern/frontend/src/pages/home.js
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
+import WorkoutDetails from '../components/workoutdetails'
+
 
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('http://localhost:5000/api/workouts');
-            const data = await response.json();
-            console.log(data);
+            //creates array of workout objects
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+            //if the response is okay, then update workouts variable to the workouts
+            if (response.ok){
+                setWorkouts(json);
+            }
         }
-    }, []);
-    return(
-        <div className = "home">
-            <h2>Home</h2>
-        </div>
-    )
 
+        fetchWorkouts();
+    }, [workouts]);
+
+    return (
+        <div className="home">
+          <div className="container">
+            {workouts && workouts.map((workout) => (
+              <WorkoutDetails key={workout._id} workout={workout}/>
+            ))}
+          </div>
+        </div>
+      );
+      
 }
 
 export default Home;
